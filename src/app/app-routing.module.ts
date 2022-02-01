71f01ddbfe2bcbc963c46af70d47caad14c40d41
Wandeson027 <w.rafael.s@hotmail.com>
2022-02-01 10:43:38 -0300

    criado repositorio Front Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo: 'itens'},
+  {
+    path: 'itens',
+    loadChildren: () => import('./itens/itens.module').then(m => m.ItensModule)
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
